39fd0522915e40990290f3723f09ef915c93b0a6
ussii <ussii@ussii.com>
2020-07-05 00:56:15 +0900

    test

--- ussii-ng-app/src/app/app.module.ts
+++ ussii-ng-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
